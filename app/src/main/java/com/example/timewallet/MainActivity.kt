fe30858f319099e5a6ffc2e1f23d471e683a54fb
Marco Martins <marco.matos@smail.inf.h-brs.de>
2023-10-25 21:03:26 +0200

    PopUp Update

--- app/src/main/java/com/example/timewallet/MainActivity.kt
+++ app/src/main/java/com/example/timewallet/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.timewallet
 
-import android.app.AlertDialog
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +8,7 @@ import com.example.timewallet.Fragments.CalenderFragment
 import com.example.timewallet.Fragments.CaptureFragment
 import com.example.timewallet.Fragments.HomeFragment
 import com.example.timewallet.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewBinding: ActivityMainBinding
@@ -18,18 +18,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
         val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
         val isFirstRun = sharedPreferences.getBoolean(FIRST_RUN, true)
 
         if (isFirstRun) {
-            // Zeigen Sie den Dialog an
-            displayFirstRunDialog()
-
-            // Setzen Sie den Wert auf false, um anzuzeigen, dass die App bereits geöffnet wurde
-            val editor = sharedPreferences.edit()
-            editor.putBoolean(FIRST_RUN, false)
-            editor.apply()
+            displayFirstRunDialog { accepted ->
+                if (accepted) {
+                    val editor = sharedPreferences.edit()
+                    editor.putBoolean(FIRST_RUN, false)
+                    editor.apply()
+                }
+            }
         }
+
         viewBinding.bottomNavigation.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.home -> fragmentsManager(HomeFragment())
@@ -54,12 +56,12 @@ class MainActivity : AppCompatActivity() {
         transaction.commit();
     }
 
-    private fun displayFirstRunDialog() {
-        val builder = AlertDialog.Builder(this, androidx.constraintlayout.widget.R.style.AlertDialog_AppCompat)
-        builder.setTitle("Willkommen")
-        builder.setMessage("Dies ist die erste Verwendung der App. Bitte gewähren Sie die erforderlichen Berechtigungen.")
+    private fun displayFirstRunDialog(callback: (accepted: Boolean) -> Unit) {
+        val builder = MaterialAlertDialogBuilder(this)
+        builder.setTitle("Herzlich Willkommen")
+        builder.setMessage("Tauchen Sie in die Zukunft\nder Zeiterfassung ein.\nIhre Zeit, Ihr Erfolg!")
         builder.setPositiveButton("OK") { dialog, which ->
-            // Aktion bei Auswahl von OK
+            callback(true)
         }
         val dialog = builder.create()
         dialog.show()
