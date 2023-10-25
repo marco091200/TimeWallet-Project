71512cfe410a9b39c3861b0ba663bca5f48bd47c
Marco Martins <marco.matos@smail.inf.h-brs.de>
2023-10-25 19:09:01 +0200

    Anfänge vom StartPopUp

--- app/src/main/java/com/example/timewallet/MainActivity.kt
+++ app/src/main/java/com/example/timewallet/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.timewallet
 
+import android.app.AlertDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,10 +12,24 @@ import com.example.timewallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewBinding: ActivityMainBinding
+    private val PREFS_NAME = "MyPrefsFile"
+    private val FIRST_RUN = "firstRun"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+        val sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val isFirstRun = sharedPreferences.getBoolean(FIRST_RUN, true)
+
+        if (isFirstRun) {
+            // Zeigen Sie den Dialog an
+            displayFirstRunDialog()
+
+            // Setzen Sie den Wert auf false, um anzuzeigen, dass die App bereits geöffnet wurde
+            val editor = sharedPreferences.edit()
+            editor.putBoolean(FIRST_RUN, false)
+            editor.apply()
+        }
         viewBinding.bottomNavigation.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.home -> fragmentsManager(HomeFragment())
@@ -36,5 +52,16 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.frameLayout,fragement)
         transaction.disallowAddToBackStack();
         transaction.commit();
+    }
+
+    private fun displayFirstRunDialog() {
+        val builder = AlertDialog.Builder(this, androidx.constraintlayout.widget.R.style.AlertDialog_AppCompat)
+        builder.setTitle("Willkommen")
+        builder.setMessage("Dies ist die erste Verwendung der App. Bitte gewähren Sie die erforderlichen Berechtigungen.")
+        builder.setPositiveButton("OK") { dialog, which ->
+            // Aktion bei Auswahl von OK
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 }
