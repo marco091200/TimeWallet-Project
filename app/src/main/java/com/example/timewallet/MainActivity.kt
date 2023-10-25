486b11d9b6652956746d6268aed1cce75890a885
Marco Martins <marco.matos@smail.inf.h-brs.de>
2023-10-25 16:13:45 +0200

    Fragmente erstellt + Navigationsbutton funktion gegeben

--- app/src/main/java/com/example/timewallet/MainActivity.kt
+++ app/src/main/java/com/example/timewallet/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.timewallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.timewallet.Fragments.CalenderFragment
+import com.example.timewallet.Fragments.CaptureFragment
+import com.example.timewallet.Fragments.HomeFragment
+import com.example.timewallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        viewBinding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> fragmentsManager(HomeFragment())
+                R.id.capture -> fragmentsManager(CaptureFragment())
+                R.id.calender -> fragmentsManager(CalenderFragment())
+                else -> false
+            }
+            return@setOnItemSelectedListener true
+        }
+        if (savedInstanceState == null) {
+            fragmentsManager(HomeFragment())
+            viewBinding.bottomNavigation.selectedItemId = R.id.home
+        }
+    }
+
+    private fun fragmentsManager(fragement:Fragment){
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+
+        transaction.replace(R.id.frameLayout,fragement)
+        transaction.disallowAddToBackStack();
+        transaction.commit();
     }
 }
