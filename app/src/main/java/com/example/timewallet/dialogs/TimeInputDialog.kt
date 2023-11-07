5eff227e4394a4951b151726a23281b3621e1f25
Marco Martins <marco.matos@smail.inf.h-brs.de>
2023-11-07 19:17:52 +0100

    TimeInputDialog überabeitet

--- app/src/main/java/com/example/timewallet/dialogs/TimeInputDialog.kt
+++ app/src/main/java/com/example/timewallet/dialogs/TimeInputDialog.kt
@@ -5,16 +5,20 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.MaterialTimePicker.INPUT_MODE_CLOCK
 import com.google.android.material.timepicker.TimeFormat
+import java.util.Calendar
 
 class TimeInputDialog {
 
     fun timeInputOpener(timeInput : TextInputEditText, parentFragmentManager: FragmentManager, titleText : String) {
         timeInput.setOnClickListener {
+            val calendar = Calendar.getInstance()
             val startTimePicker = MaterialTimePicker
                 .Builder()
                 .setInputMode(INPUT_MODE_CLOCK)
                 .setTimeFormat(TimeFormat.CLOCK_24H)
                 .setTitleText(titleText)
+                .setHour(calendar.get(Calendar.HOUR_OF_DAY))
+                .setMinute(calendar.get(Calendar.MINUTE))
                 .build()
             startTimePicker.show(parentFragmentManager, "timePicker")
             startTimePicker.addOnPositiveButtonClickListener {
