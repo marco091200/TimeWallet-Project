1dc87b2e41075a3d58666aa98fcc22c85174589d
Marco Martins <marco.matos@smail.inf.h-brs.de>
2023-11-09 13:19:37 +0100

    Neue Klasse für PDF-Download + Gradle Itext Pdf hinzugefügt Version 7.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.timewallet"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.itextpdf:itext7-core:7.2.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
---
+++ app/src/main/java/com/example/timewallet/pdf/WorkRecordListToPDF.kt
@@ -0,0 +1,4 @@
+package com.example.timewallet.pdf
+
+class WorkRecordListToPDF {
+}
