44c5c61e0af6eb1f0fa7fea8cf400b913a83d1da
Marco Martins <marco.matos@smail.inf.h-brs.de>
2024-01-02 18:07:58 +0100

    PDF Ergänzung Name und Arbeitsstunden

--- app/src/main/java/com/example/timewallet/pdf/WorkRecordListToPDF.kt
+++ app/src/main/java/com/example/timewallet/pdf/WorkRecordListToPDF.kt
@@ -3,6 +3,7 @@ package com.example.timewallet.pdf
 import android.content.Context
 import android.os.Environment
 import android.widget.Toast
+import com.example.timewallet.controls.UserControl
 import com.example.timewallet.controls.WorkRecordControl
 import com.example.timewallet.record.WorkRecord
 import com.itextpdf.kernel.pdf.PdfDocument
@@ -17,62 +18,91 @@ import java.util.Locale
 
 class WorkRecordListToPDF {
     private val workRecordControl = WorkRecordControl()
-    fun createPDFPeriod(context: Context, workRecords: List<WorkRecord>, startDate: String, endDate:String): String {
-        try{
-        // Erstelle ein Dateiobjekt für den Download-Ordner
-        val downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+    fun createPDFPeriod(
+        context: Context,
+        workRecords: List<WorkRecord>,
+        startDate: String,
+        endDate: String
+    ): String {
+        try {
+            // Erstelle ein Dateiobjekt für den Download-Ordner
+            val downloadDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
 
-        val uniqueRecords = removeDuplicateRecords(workRecords)
+            val uniqueRecords = removeDuplicateRecords(workRecords)
             val filteredRecords = uniqueRecords.filter { it.date in startDate..endDate }
-            val sortedWorkRecords = filteredRecords.sortedBy { SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).parse(it.date) }
+            val sortedWorkRecords = filteredRecords.sortedBy {
+                SimpleDateFormat(
+                    "dd.MM.yyyy",
+                    Locale.getDefault()
+                ).parse(it.date)
+            }
 
+            val userControl = UserControl(context)
+            val user = userControl.readUserFromTxt()
 
             // Erstelle eine PDF-Datei im Download-Ordner
-        val pdfFileName = "Arbeitszeit-$startDate-$endDate.pdf"
-        val pdfFile = File(downloadDir, pdfFileName)
+            val pdfFileName = "Arbeitszeit-$startDate-$endDate.pdf"
+            val pdfFile = File(downloadDir, pdfFileName)
 
-        val writer = PdfWriter(FileOutputStream(pdfFile))
+            val writer = PdfWriter(FileOutputStream(pdfFile))
 
-        // Erstelle ein PdfDocument
-        val pdf = PdfDocument(writer)
+            // Erstelle ein PdfDocument
+            val pdf = PdfDocument(writer)
 
-        // Erstelle ein Document
-        val document = Document(pdf)
+            // Erstelle ein Document
+            val document = Document(pdf)
 
-        // Erstelle eine Tabelle mit 5 Spalten (Datum, Startzeit, Endzeit, Arbeitsstunden, Bemerkung)
-        val table = Table(floatArrayOf(100f, 100f, 100f, 100f, 100f))
+            if (user?.benutzerName != "") {
+                document.add(Paragraph("Benutzername: " + user?.benutzerName))
+            }
+            if (user?.monatlicheArbeitsstunden != "") {
+                document.add(Paragraph("Monatliche Arbeitsstunden: " + user?.monatlicheArbeitsstunden))
+            }
+            if (user != null) {
+                document.add(Paragraph("\n"))
+            }
 
-        // Füge Header-Zellen hinzu
-        table.addHeaderCell("Datum")
-        table.addHeaderCell("Startzeit")
-        table.addHeaderCell("Endzeit")
-        table.addHeaderCell("Arbeitsstunden")
-        table.addHeaderCell("Bemerkung")
+            val table = Table(floatArrayOf(100f, 100f, 100f, 100f, 100f))
 
-        // Füge Daten-Zellen hinzu
-        for (workRecord in sortedWorkRecords) {
-            table.addCell(workRecord.date)
-            table.addCell(workRecord.startTime)
-            table.addCell(workRecord.endTime)
-            table.addCell(workRecord.workedHours)
-            table.addCell(workRecord.chipInput)
-        }
+            // Füge Header-Zellen hinzu
+            table.addHeaderCell("Datum")
+            table.addHeaderCell("Startzeit")
+            table.addHeaderCell("Endzeit")
+            table.addHeaderCell("Arbeitsstunden")
+            table.addHeaderCell("Bemerkung")
 
-        // Füge die Tabelle zum Dokument hinzu
+            // Füge Daten-Zellen hinzu
+            for (workRecord in sortedWorkRecords) {
+                table.addCell(workRecord.date)
+                table.addCell(workRecord.startTime)
+                table.addCell(workRecord.endTime)
+                table.addCell(workRecord.workedHours)
+                table.addCell(workRecord.chipInput)
+            }
+
+            // Füge die Tabelle zum Dokument hinzu
             document.add(table)
             // Füge einen Abstand (z.B. 10 Einheiten) zwischen Tabelle und Textzeile hinzu
             document.add(Paragraph("\n"))
 
             // Füge die einzelne Textzeile zum Dokument hinzu
-            document.add(Paragraph("Ihre Arbeitsstunden in diesem Zeitraum betragen insgesamt: " + workRecordControl.hoursMonth(workRecords)))
-        // Schließe das Dokument
+            document.add(
+                Paragraph(
+                    "Ihre Arbeitsstunden in diesem Zeitraum betragen insgesamt: " + workRecordControl.hoursMonth(
+                        workRecords
+                    )
+                )
+            )
+            // Schließe das Dokument
             document.close()
-            Toast.makeText(context, "PDF erfolgreich erstellt: $pdfFileName", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "PDF erfolgreich erstellt: $pdfFileName", Toast.LENGTH_SHORT)
+                .show()
             // Gib eine Erfolgsmeldung aus
             println("PDF erfolgreich erstellt: $pdfFileName")
 
             return pdfFileName
-        }catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
             Toast.makeText(context, "Fehler beim Erstellen der PDF!", Toast.LENGTH_SHORT).show()
             println("Fehler beim Erstellen der PDF: ${e.message}")
